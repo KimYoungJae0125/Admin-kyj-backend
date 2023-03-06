ffd27b39c1bacf06949c1a7ec7a67ed6733ce7ef
kyj <uni0125@naver.com>
2023-03-06 18:07:33 +0900

    Fix CustomSort
    layoutOrder 제외하고도 추가로 정렬할 컬럼이 있을 경우 추가하는 로직 추가

--- src/main/java/kr/kimyoungjae/admin/common/conditions/CustomSort.java
+++ src/main/java/kr/kimyoungjae/admin/common/conditions/CustomSort.java
@@ -7,4 +7,15 @@ public class CustomSort {
     public static Sort layoutSort() {
         return Sort.by(Sort.Direction.ASC, "layoutOrder");
     }
+    public static Sort layoutSort(String... properties) {
+        String[] newProperties = new String[properties.length+1];
+        int i = 0;
+        for(; i < properties.length; i++) {
+            newProperties[i] = properties[i];
+        }
+        newProperties[i] = "layoutOrder";
+        return Sort.by(Sort.Direction.ASC, newProperties);
+
+    }
+
 }
