cf17521c486ac459f569ae8ab3d66738753c1a35
kyj <uni0125@naver.com>
2023-03-09 19:32:37 +0900

    Fix SecurityConfig
    변경
    - accessDeniedHandler
    - authenticationEntryPoint

--- src/main/java/kr/kimyoungjae/admin/common/config/SecurityConfig.java
+++ src/main/java/kr/kimyoungjae/admin/common/config/SecurityConfig.java
@@ -1,14 +1,26 @@
 package kr.kimyoungjae.admin.common.config;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import kr.kimyoungjae.admin.common.utils.JsonUtils;
+import kr.kimyoungjae.admin.common.utils.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
@@ -16,7 +28,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
-    private final SecurityExceptionHandler securityExceptionHandler;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -33,10 +45,34 @@ public class SecurityConfig {
                             .anyRequest().authenticated()
                 )
                 .exceptionHandling(handler ->
-                        handler.accessDeniedHandler(securityExceptionHandler)
-                                .authenticationEntryPoint(securityExceptionHandler))
+                        handler.accessDeniedHandler(accessDeniedHandler())
+                                .authenticationEntryPoint(authenticationEntryPoint()))
                 .build();
     }
 
+    private void createErrorMessage(HttpServletRequest request, HttpServletResponse response, String exceptionMessage) throws IOException {
+        String token = jwtTokenProvider.getToken(request.getCookies());
+        String roles = "NO_ROLE";
+        if(jwtTokenProvider.validate(token)) {
+            roles = jwtTokenProvider.getAuthentication(token).getAuthorities().stream().map(String::valueOf).collect(Collectors.joining(", "));
+        }
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        String json = JsonUtils.objectToJson(new ErrorMessage(roles, exceptionMessage));
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        PrintWriter writer = response.getWriter();
+        writer.write(json);
+        writer.flush();
+    }
+
+    private AuthenticationEntryPoint authenticationEntryPoint() {
+        return (request, response, authException) -> createErrorMessage(request, response, authException.getMessage());
+    }
+
+    private AccessDeniedHandler accessDeniedHandler() {
+        return (request, response, accessDeniedException) -> createErrorMessage(request, response, accessDeniedException.getMessage());
+    }
+
+    record ErrorMessage(String roles, String exceptionMessage) {
+    }
 
 }
